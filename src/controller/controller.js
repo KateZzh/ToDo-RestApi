74f4583eb43e2f50dba54253a1f042a79359890d
KateZzh <ekaterin.pro2@gmail.com>
2024-01-31 19:58:24 +0300

    added eslint, prettier, env, buildResponse

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,45 +1,46 @@
 const express = require('express');
+const buildResponse = require('../helper/buildResponse');
 const { createTask, getTask, getTaskById, updateTask, deleteTask } = require('../service/service');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
   try {
-    res.status(200).send(await createTask(req.body));
+    buildResponse(res, 200, await createTask(req.body));
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 
 router.get('/', async (req, res) => {
   try {
-    res.status(200).send(await getTask());
+    buildResponse(res, 200, await getTask());
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 
 router.get('/:_id', async (req, res) => {
   try {
-    res.status(200).send(await getTaskById(req.params._id));
+    buildResponse(res, 200, await getTaskById(req.params._id));
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 
 router.put('/:_id', async (req, res) => {
   try {
-    res.status(200).send(await updateTask(req.params._id, req.body));
+    buildResponse(res, 200, await updateTask(req.params._id, req.body));
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 
 router.delete('/:_id', async (req, res) => {
   try {
-    res.status(200).send(await deleteTask(req.params._id));
+    buildResponse(res, 200, await deleteTask(req.params._id));
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 
--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/todo_restapi');
+mongoose.connect(process.env.MONGODB);
 
 const Table = mongoose.model('tasks', {
   title: String,
