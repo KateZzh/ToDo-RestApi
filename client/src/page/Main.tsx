1cfccebc00d7edcc83fbab3314b6dd1b657085b0
KateZzh <ekaterin.pro2@gmail.com>
2024-02-02 12:38:35 +0300

    client Main html+scss

--- client/src/page/Main.tsx
+++ client/src/page/Main.tsx
@@ -1,5 +1,9 @@
 import style from './Main.style.module.scss';
 import detectiveImg from '../assets/detective.png';
+// import checkMarkImg from '../assets/checkMark.svg';
+import checkMarkEmptyImg from '../assets/checkMarkEmpty.svg';
+import pencilImg from '../assets/pencil.svg';
+import garbageCanImg from '../assets/garbageCan.svg';
 
 export default function Main() {
   return (
@@ -17,6 +21,23 @@ export default function Main() {
       <div className={style.emptyWrapper}>
         <img src={detectiveImg} alt='detectiveImg' />
         <p>Empty...</p>
+      </div>
+
+      <div className={style.existingNotesWrapper}>
+        <div className={style.generalNoteWrapper}>
+          <div className={style.noteWrapper}>
+            <img className={style.checkMarkImg} src={checkMarkEmptyImg} alt='checkMarkImg' />
+            <p className={style.noteTitle}>Note #1</p>
+            <p className={style.noteDescription}>Description</p>
+          </div>
+
+          <div className={style.iconsNoteWrapper}>
+            <img className={style.pencilImg} src={pencilImg} alt='pencilImg' />
+            <img className={style.garbageCanImg} src={garbageCanImg} alt='garbageCanImg' />
+          </div>
+        </div>
+
+        <div className={style.line}></div>
       </div>
     </div>
   );
